6fa93f9a62e77855cef8606fd78fadd37ed7b2e9
flavionevesfsn <flavioneves89@gmail.com>
2021-07-28 23:34:27 -0300

    Enviando os arquivos do projeto

---
+++ src/main/kotlin/com/flavio/colections/TestesIntArrays.kt
@@ -0,0 +1,29 @@
+package com.flavio.colections
+
+fun main() {
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values) {
+        println(valor)
+    }
+    println("-----------------------------------------------------------")
+    values.forEach { valor ->
+        println(valor)
+    }
+    println("-----------------------------------------------------------")
+
+    for (index in values.indices) {
+        println(values[index])
+    }
+
+    values.sort()
+    for (valor in values) {
+        println(valor)
+    }
+}
